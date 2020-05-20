990e405accf4891e6dbf37a580f8f8d2ec8a8190
S Mugenzi <smugenzi@Ss-MacBook-Air.local>
2020-05-20 17:08:36 -0400

    CRA file setup

---
+++ plugged/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Plugged 🔌</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
